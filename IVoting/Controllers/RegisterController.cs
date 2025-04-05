9ea42f19b1d7ae21c161efea00bd2060bba88155
Kaique Albuquerque <kaique-albuquerque@hotmail.com>
2025-04-05 18:26:16 -0300

    Add project files.

---
+++ IVoting/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+using IVoting.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IVoting.Controllers
+{
+	public class RegisterController : Controller
+	{
+		private readonly ILogger<RegisterController> _logger;
+
+		public RegisterController(ILogger<RegisterController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ IVoting/Models/RegisterPersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IVoting.Models
+{
+	public class RegisterPersonModel
+	{
+		public string Email { get; set; }
+		public string Name { get; set; }
+	}
+}
